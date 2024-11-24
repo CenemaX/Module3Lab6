0f0da511e92ed88d0f404891d83183608fcb88a1
RedaM4 <xxred49xx@gmail.com>
2024-11-24 15:36:32 +0300

    Fixed adder  (i hope it works)

--- Lab06.srcs/sources_1/new/FixedAdder.sv
+++ Lab06.srcs/sources_1/new/FixedAdder.sv
@@ -20,8 +20,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module FixedAdder #(parameter n=8, parameter m1=8,parameter m2=8)(
-input 
+module FixedAdder #(parameter n1 = 8, parameter n2 = 8, parameter m1 = 8, parameter m2 = 8)(
+    input logic [n1-1:0] a,        // Input a with n1 bits
+    input logic [n2-1:0] b,        // Input b with n2 bits
+    output logic [(n1 > n2 ? n1 : n2)-1:0] res // Output res with the maximum width
+);
 
-    );
+    // Determine the maximum width
+    localparam int max_width = (n1 > n2) ? n1 : n2;
+
+    // Perform addition and assign to res
+    always_comb begin
+        res = a + b; // Fixed-point addition
+    end
+
 endmodule
