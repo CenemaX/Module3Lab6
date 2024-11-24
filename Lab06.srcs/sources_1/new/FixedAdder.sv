7f0fbe9e4d02e8f8facc3ebf2e748f5a55e810dc
RedaM4 <xxred49xx@gmail.com>
2024-11-24 14:57:29 +0300

    initiat

---
+++ Lab06.srcs/sources_1/new/FixedAdder.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/24/2024 02:51:42 PM
+// Design Name: 
+// Module Name: FixedAdder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FixedAdder #(parameter n=8, parameter m1=8,parameter m2=8)(
+input 
+
+    );
+endmodule
